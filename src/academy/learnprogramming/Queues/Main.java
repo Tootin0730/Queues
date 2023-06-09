73265fd8e8d0223bb7b45682ab33db2c7e4cdb78
Tootin0730 <yisan1004@gmail.com>
2023-06-09 12:09:11 +0900

    Queues

---
+++ src/academy/learnprogramming/Queues/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming.Queues;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(10);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.add(marySmith);
+        queue.add(mikeWilson);
+        queue.add(billEnd);
+
+        queue.remove();
+        queue.remove();
+        queue.printQueue();
+
+    }
+}
